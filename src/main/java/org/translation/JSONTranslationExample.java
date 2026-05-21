6c326f440eada6db1da9b651450af924421d0b13
AdamA <adam.alagil@mail.utoronto.ca>
2026-05-21 19:24:58 -0400

    Fixed some checkstyles

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -23,7 +23,8 @@ public class JSONTranslationExample {
             // which we then create a new JSONArray object from.
             // TODO CheckStyle: Line is longer than 120 characters
             //                  (note: you can split a line such that the next line starts with a .method()... call
-            String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource("sample.json").toURI()));
+            String jsonString = Files.readString(Paths.get(getClass().getClassLoader()
+                    .getResource("sample.json").toURI()));
             this.jsonArray = new JSONArray(jsonString);
         }
         catch (IOException | URISyntaxException ex) {
@@ -38,7 +39,9 @@ public class JSONTranslationExample {
     public String getCanadaCountryNameSpanishTranslation() {
 
         // TODO Checkstyle: '30' is a magic number.
-        JSONObject canada = jsonArray.getJSONObject(30);
+        int canada_json_number = 30;
+
+        JSONObject canada = jsonArray.getJSONObject(canada_json_number);
         return canada.getString("es");
     }
 
